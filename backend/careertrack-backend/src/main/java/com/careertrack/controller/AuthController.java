2b1e335310491a1e28ca96723f08b05e7a8769d8
Julian Ramirez <sirlianjobs@gmail.com>
2026-06-11 19:34:01 -0500

    feat: create AuthController

---
+++ backend/careertrack-backend/src/main/java/com/careertrack/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.careertrack.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.careertrack.dto.UserRegisterRequest;
+import com.careertrack.dto.UserResponse;
+import com.careertrack.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(
+        @Valid @RequestBody UserRegisterRequest request) {
+    	//Método para manejar la solicitud de registro de un nuevo Usuario.
+        UserResponse response = userService.register(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
--- backend/careertrack-backend/src/main/java/com/careertrack/service/UserService.java
+++ backend/careertrack-backend/src/main/java/com/careertrack/service/UserService.java
@@ -3,11 +3,15 @@ package com.careertrack.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.careertrack.dto.UserRegisterRequest;
+import com.careertrack.dto.UserResponse;
 import com.careertrack.entity.User;
 
 public interface UserService {
 
     User saveUser(User user);
+    
+    UserResponse register(UserRegisterRequest request);
 
     Optional<User> findById(Long id);
 
