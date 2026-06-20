f479bfccdc75ddc5ff2ac47a9a47b0c729860900
Julian Ramirez <sirlianjobs@gmail.com>
2026-06-20 15:24:47 -0500

    feat: create validation errors in GlobalException

--- backend/careertrack-backend/src/main/java/com/careertrack/exception/GlobalExceptionHandler.java
+++ backend/careertrack-backend/src/main/java/com/careertrack/exception/GlobalExceptionHandler.java
@@ -1,19 +1,21 @@
 package com.careertrack.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 //Controlador de excepciones global para manejar errores comunes en la aplicación.
 	
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse>
-    handleResourceNotFound(ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponse>handleResourceNotFound(ResourceNotFoundException ex) {
     	//Metodo para excepción de recurso no encontrado que construye una respuesta de error con detalles.
         ErrorResponse response = ErrorResponse.builder()
                         .timestamp(LocalDateTime.now())
@@ -27,8 +29,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse>
-    handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
+    public ResponseEntity<ErrorResponse>handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
     	//Metodo para excepción de email ya existente que construye una respuesta de error con detalles.
         ErrorResponse response = ErrorResponse.builder()
                         .timestamp(LocalDateTime.now())
@@ -41,4 +42,18 @@ public class GlobalExceptionHandler {
                 .body(response);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+    	//Método que captura errores de validación de DTOs
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
